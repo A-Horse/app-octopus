2e2839dba6991fb142bec8907c64a88d892bc0fb
fwchen <crystal.tyan@gmail.com>
2017-03-22 21:33:40 +0800

    initial commit

--- index.ios.js
+++ index.ios.js
@@ -12,42 +12,22 @@ import {
   View
 } from 'react-native';
 
-export default class OctopusApp extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+import { TabNavigator } from 'react-navigation';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+import TodoScreen from './screen/Todo.js';
+import TaskScreen from './screen/Task.js';
+
+const MyApp = TabNavigator({
+  Home: {
+    screen: TaskScreen,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  Notifications: {
+    screen: TodoScreen,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+}, {
+  tabBarOptions: {
+    activeTintColor: '#e91e63',
   },
 });
 
-AppRegistry.registerComponent('OctopusApp', () => OctopusApp);
+AppRegistry.registerComponent('OctopusApp', () => MyApp);
