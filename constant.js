e4427b1bfaaabd328a19f187254dd26a59f1ce4f
fwchen <crystal.tyan@gmail.com>
2017-03-31 23:47:30 +0800

    initial commit

--- constant.js
+++ constant.js
@@ -1,6 +1,7 @@
 
 // export const apiUrl = 'http://localhost:5500/api';
-export const apiUrl = 'http://127.0.0.1/api';
+// export const apiUrl = 'http://127.0.0.1/api';
+export const apiUrl = 'http://octopus.com/api';
 
 export const JWT = "jwt";
 
