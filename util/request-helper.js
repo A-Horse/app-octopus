ab35179e43408bc66e82dcb5f3300b0a46fbf68c
fwchen <crystal.tyan@gmail.com>
2017-07-01 15:39:38 +0800

    fix crash issue

--- util/request-helper.js
+++ util/request-helper.js
@@ -3,7 +3,7 @@ import { Alert } from 'react-native';
 export function handleEpicError(error, caught) {
   if (__DEV__) {
     console.error(error, caught);
-    return caught; // TODO ? return 有毛用
+    return caught;
   } else {
     Alert.alert(
       'Unexpected error occurred',
@@ -12,4 +12,5 @@ export function handleEpicError(error, caught) {
         `
     );
   }
+  return caught; // must return Observable http://reactivex.io/rxjs/class/es6/Observable.js~Observable.html#instance-method-catch
 }
