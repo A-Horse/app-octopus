d4c36bc9f117f2f78f95e4a946cee9df44add620
fwchen <crystal.tyan@gmail.com>
2017-07-01 15:40:00 +0800

    format

--- epic/auth.js
+++ epic/auth.js
@@ -1,24 +1,31 @@
-import { AUTH_REQUEST,
-         SIGNUP_REQUEST,
-         authSuccess,
-         signupSuccess
-       } from '../action/auth';
+import {
+  AUTH_REQUEST,
+  SIGNUP_REQUEST,
+  authSuccess,
+  signupSuccess
+} from '../action/auth';
 import { makeServerApi } from '../util/api-maker';
 import { ajax } from 'rxjs/observable/dom/ajax';
 import { handleEpicError } from '../util/request-helper';
 
 export const auth = action$ =>
-  action$.ofType(AUTH_REQUEST)
-  .mergeMap(action => {
-    return ajax.post(makeServerApi('signin'), action.playload)
-      .map(response => response.response)
-      .map(authSuccess);
-  }).catch(handleEpicError);
+  action$
+    .ofType(AUTH_REQUEST)
+    .mergeMap(action => {
+      return ajax
+        .post(makeServerApi('signin'), action.playload)
+        .map(response => response.response)
+        .map(authSuccess);
+    })
+    .catch(handleEpicError);
 
 export const signup = action$ =>
-  action$.ofType(SIGNUP_REQUEST)
-  .mergeMap(action => {
-    return ajax.post(makeServerApi('signup'), action.playload)
-      .map(response => response.response)
-      .map(signupSuccess);
-  }).catch(handleEpicError);
+  action$
+    .ofType(SIGNUP_REQUEST)
+    .mergeMap(action => {
+      return ajax
+        .post(makeServerApi('signup'), action.playload)
+        .map(response => response.response)
+        .map(signupSuccess);
+    })
+    .catch(handleEpicError);
