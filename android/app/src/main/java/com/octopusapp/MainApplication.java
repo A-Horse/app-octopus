c2719ce93ebb759a4341fb0873ce02d136479f4d
fwchen <crystal.tyan@gmail.com>
2017-08-14 10:29:24 +0800

    refactor redux flux for one

--- android/app/src/main/java/com/octopusapp/MainApplication.java
+++ android/app/src/main/java/com/octopusapp/MainApplication.java
@@ -3,8 +3,6 @@ package com.octopusapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
-import com.reactnativenavigation.NavigationReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,9 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new ReactNativePushNotificationPackage(),
-            new NavigationReactPackage()
+          new MainReactPackage()
       );
     }
   };
