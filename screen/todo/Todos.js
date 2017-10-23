497365ad955ca68c1e012b2777602e7781ec952f
fwchen <crystal.tyan@gmail.com>
2017-10-23 11:14:54 +0800

    udpate todos page

--- screen/todo/Todos.js
+++ screen/todo/Todos.js
@@ -13,20 +13,28 @@ import { ScreenBgColor } from '../../constant';
 import { makeActionRequestCollection } from '../../action/actioner';
 import { navigatorStyle } from '../../navigation-setup';
 
-const getAllTodos = (state, props) => {
+const getTodos = (state, props) => {
   const { meta } = props;
   const { entities, results } = state.todo;
   const todoResults = results[meta.id] || [];
   return todoResults.map(id => entities[id]);
 };
 
-const getTodos = createSelector([getAllTodos], R.sort(R.prop('isDone')));
-
 const mapStateToProps = (state, props) => {
   return {
     user: state.auth.user,
     userId: state.auth.user.id,
-    todos: getTodos(state, props)
+    unDoneTodos: createSelector(
+      [getTodos],
+      R.compose(
+        R.sort(R.descend(R.prop('created_at'))),
+        R.filter(R.compose(R.not, R.prop('isDone')))
+      )
+    )(state, props),
+    doneTodos: createSelector(
+      [getTodos],
+      R.compose(R.sort(R.descend(R.prop('created_at'))), R.filter(R.prop('isDone')))
+    )(state, props)
   };
 };
 
@@ -43,6 +51,10 @@ class Todos extends Component {
   static propTypes = {
     actions: PropTypes.object.isRequired,
     navigator: PropTypes.object.isRequired
+  };
+
+  state = {
+    showAll: false
   };
 
   constructor(props) {
@@ -65,9 +77,9 @@ class Todos extends Component {
     const userId = this.props.user.id;
     this.props.actions.ADD_TODO_REQUEST({
       todoBoxId: this.props.meta.id,
-      content: this.refs.creater.state.content
+      content: this.refCreater.state.content
     });
-    this.refs.creater.clear();
+    this.refCreater.clear();
   }
 
   componentDidMount() {
@@ -90,47 +102,69 @@ class Todos extends Component {
     });
   }
 
+  renderList(category) {
+    return (
+      <FlatList
+        data={this.props[category]}
+        renderItem={({ item }) => (
+          <Todo
+            boxId={this.props.meta.id}
+            user={this.props.user}
+            meta={this.props.meta}
+            key={item.id}
+            todo={item}
+            navigator={this.props.navigator}
+            updateTodo={data => this.props.actions.UPDATE_TODO_REQUEST({ ...data, id: item.id })}
+          />
+        )}
+        keyExtractor={item => item.id}
+        ItemSeparatorComponent={() => (
+          <View
+            style={{
+              height: 1,
+              width: '100%',
+              backgroundColor: '#f8f8f8'
+            }}
+          />
+        )}
+      />
+    );
+  }
+
   render() {
-    const userId = this.props.user.id;
     return (
       <View style={styles.container}>
         <ScrollView style={styles.scrollView}>
           <TodoCreater
-            ref="creater"
+            ref={ref => (this.refCreater = ref)}
             addCreateTodoButton={this.addCreateTodoButton}
             clearNavButton={this.clearNavButton}
             createTodo={this.createTodo}
           />
           <View>
-            <FlatList
-              data={this.props.todos}
-              renderItem={({ item }) => (
-                <Todo
-                  boxId={this.props.meta.id}
-                  user={this.props.user}
-                  meta={this.props.meta}
-                  key={item.id}
-                  todo={item}
-                  navigator={this.props.navigator}
-                  updateTodo={data =>
-                    this.props.actions.UPDATE_TODO_REQUEST({ ...data, id: item.id })}
-                />
-              )}
-              keyExtractor={item => item.id}
-              ItemSeparatorComponent={() => (
-                <View
-                  style={{
-                    height: 1,
-                    width: '100%',
-                    backgroundColor: '#f8f8f8'
-                  }}
-                />
-              )}
-            />
-            <TouchableOpacity style={{ flexDirection: 'column' }}>
-              <Icon name="playlist-add-check" size={30} color="#900" />
-              <Text>Show Done Todos</Text>
+            {this.renderList('unDoneTodos')}
+
+            <TouchableOpacity
+              onPress={() => this.setState({ showAll: !this.state.showAll })}
+              style={{
+                paddingTop: 9,
+                paddingLeft: 9,
+                flex: 1,
+                flexWrap: 'wrap',
+                flexDirection: 'row',
+                alignItems: 'center'
+              }}
+            >
+              <Icon
+                style={{ marginRight: 3 }}
+                name="playlist-add-check"
+                size={30}
+                color="#80be65"
+              />
+              <Text style={{ color: '#555' }}>Show Done Todos</Text>
             </TouchableOpacity>
+
+            {this.state.showAll && this.renderList('doneTodos')}
           </View>
         </ScrollView>
       </View>
