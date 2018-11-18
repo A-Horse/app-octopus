f4161b96e4b4ec83e3e03bbb7170f05cd363db54
fwchen <crystal.tyan@gmail.com>
2018-11-18 17:55:15 +0800

    change server url

--- src/env/env.prod.js
+++ src/env/env.prod.js
@@ -1,2 +1,2 @@
-export const API_BASE = 'https://www.octopuese.xyz/api';
-export const SERVER_BASE = 'https://www.octopuese.xyz/';
+export const API_BASE = 'https://work.octopuese.xyz/api';
+export const SERVER_BASE = 'https://work.octopuese.xyz/';
