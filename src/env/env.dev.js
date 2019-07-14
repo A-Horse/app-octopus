fa4295bb1c898e526729b861377d765a9207282d
fwchen <me@chenfangwei.com>
2019-07-14 23:53:47 +0800

    change some dev ip

--- src/env/env.dev.js
+++ src/env/env.dev.js
@@ -1,2 +1,2 @@
-export const API_BASE = 'http://192.168.50.77:8888/api';
-export const SERVER_BASE = 'http://192.168.50.77:9000/';
+export const API_BASE = 'http://192.168.50.223:8888/api';
+export const SERVER_BASE = 'http://192.168.50.223:9000/';
